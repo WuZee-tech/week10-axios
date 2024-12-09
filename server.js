0ced7a11560be0bfe6c727a7e76d32b21d112550
Allan o <allanojala1@gmail.com>
2024-12-09 17:02:55 +0200

    merging two projects

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res)=>{
     let url = 'https://api.themoviedb.org/3/movie/123?api_key=4ca94f8b470d7e34bd3f59c3914295c8';
@@ -12,9 +13,11 @@ app.get('/', (req, res)=>{
         let data = response.data;
         let releaseDate = new Date(data.release_date).getFullYear();
         let genresToDisplay = '';
+        
         data.genres.forEach(genre => {
             genresToDisplay = genresToDisplay + `${genre.name}, `;
         });
+        
         let genresUpdated = genresToDisplay.slice(0, -2) + '.';
         let posterUrl = `https://www.themoviedb.org/t/p/w600_and_h900_bestv2${data.poster_path}`;
 
@@ -31,6 +34,57 @@ app.get('/', (req, res)=>{
         });
     });
    
+});
+
+app.get ('/search', (req, res) =>{
+    res.render('search', { movieDetails: null });
+});
+
+app.post('/search', (req, res) =>{
+
+    let userMovieTitle = req.body.movieTitle;
+    
+
+    let moveiUrl = `https://api.themoviedb.org/3/search/movie?api_key=4ca94f8b470d7e34bd3f59c3914295c8&query=${userMovieTitle}`;
+    let genresUrl = 'https://api.themoviedb.org/3/genre/movie/list?api_key=4ca94f8b470d7e34bd3f59c3914295c8&language=en-US';
+
+    let endpoints = [moveiUrl, genresUrl];
+
+    axios.all(endpoints.map((endpoint) => axios.get(endpoint)))
+    .then(axios.spread((movie, genres) => {
+
+        const [movieRaw] = movie.data.results
+        
+        let movieGenreIds = movieRaw.genre_ids;
+        let movieGenres = genres.data.genres;
+        let movieGenresArray = [];
+        for (let i = 0; i < movieGenreIds.length; i++){
+            for(let j = 0; j < movieGenres.length; j++){
+                if(movieGenreIds[i] === movieGenres[j].id){
+                    movieGenresArray.push(movieGenres[j].name);
+                }
+            }
+        }
+
+        let genresToDisplay = '';
+        movieGenresArray.forEach(genre => {
+            genresToDisplay += `${genre}, `; 
+        });
+
+        genresToDisplay = genresToDisplay.slice(0, -2) + '.';
+
+        let movieData = {
+            title: movieRaw.title,
+            year: new Date(movieRaw.release_date).getFullYear(),
+            genres: genresToDisplay,
+            overview: movieRaw.overview,
+            posterUrl: `https://image.tmdb.org/t/p/w500${movieRaw.poster_path}`
+        };
+    
+        res.render('search', {movieDetails: movieData});
+
+    }));
+
 });
 
 app.listen (process.env.PORT || 3000, ()=>{
